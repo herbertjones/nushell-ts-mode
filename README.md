b68d0ff370fb8cfcf547d36d5dc648e5f22ce587
Herbert Jones <jones.herbert@gmail.com>
2023-08-05 09:48:48 -0500

    feat: add elisp and readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# Nushell Tree-Sitter mode
+
+[![License GPL 3](https://img.shields.io/badge/license-GPL_3-green.svg)](http://www.gnu.org/licenses/gpl-3.0.txt)
+
+A mode for Nushell that uses tree-sitter.
+
+* https://www.nushell.sh/
+* https://github.com/nushell/tree-sitter-nu
+* https://tree-sitter.github.io/tree-sitter/
+
+
+## Features
+
+* Syntax highlighting
+* Indentation
+
+![example highlighting](images/sample.png)
+
+
+## Installation
+
+### Build nu-tree-sitter
+
+1. Run `treesit-install-language-grammar`
+2. Select "nu"
+3. Use URL https://github.com/nushell/tree-sitter-nu
+4. Pick "src"
+
+
+### Install package
+
+You will need to clone the repo and load it manually or use whatever package manger you use.
+
+```emacs-lisp
+(straight-use-package
+ '(nushell-ts-mode :type git :host github :repo "herbertjones/nushell-ts-mode"))
+```
---
+++ nushell-ts-mode.el
@@ -0,0 +1,251 @@
+;;; nushell-ts-mode.el --- tree-sitter support for Nushell  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2023 Free Software Foundation, Inc.
+
+;; Author     : Herbert Jones <jones.herbert@gmail.com>
+;; Maintainer : Herbert Jones <jones.herbert@gmail.com>
+;; Created    : August 2023
+;; Homepage   : https://github.com/herbertjones/nushell-ts-mode
+;; Keywords   : nu nushell languages tree-sitter
+
+;; Package-Version: 0.0.1
+;; Package-Requires: ((emacs "29.1"))
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Still in early stages.  I haven't used it much, but I wanted something that
+;; was usable.
+
+;;; Code:
+
+(require 'treesit)
+
+(declare-function treesit-font-lock-rules "treesit.c")
+(declare-function treesit-major-mode-setup "treesit.c")
+(declare-function treesit-node-child-by-field-name "treesit.c")
+(declare-function treesit-node-child "treesit.c")
+(declare-function treesit-node-text "treesit.c")
+(declare-function treesit-node-type "treesit.c")
+(declare-function treesit-parser-create "treesit.c")
+(declare-function treesit-ready-p "treesit.c")
+
+(defcustom nushell-ts-mode-indent-offset 2
+  "Number of spaces for each indentation step in `nushell-ts-mode'."
+  :version "29.1"
+  :type 'integer
+  :safe 'integerp
+  :group 'nushell)
+
+(defvar nushell-ts-mode--operators nil "Nushell operators for tree-sitter font-locking.")
+(setq nushell-ts-mode--operators
+      '("+"           ; add
+        "-"           ; subtract
+        "*"           ; multiply
+        "/"           ; divide
+        "//"          ; floor division
+        "mod"         ; modulo
+        "**"          ; exponentiation (power)
+        "=="          ; equal
+        "!="          ; not equal
+        "<"           ; less than
+        "<="          ; less than or equal
+        ">"           ; greater than
+        ">="          ; greater than or equal
+        "=~"          ; regex match / string contains another
+        "!~"          ; inverse regex match / string does not contain another
+        "in"          ; value in list
+        "not-in"      ; value not in list
+        "not"         ; logical not
+        "and"         ; and two Boolean expressions (short-circuits)
+        "or"          ; or two Boolean expressions (short-circuits)
+        "xor"         ; exclusive or two boolean expressions
+        "bit-or"      ; bitwise or
+        "bit-xor"     ; bitwise xor
+        "bit-and"     ; bitwise and
+        "bit-shl"     ; bitwise shift left
+        "bit-shr"     ; bitwise shift right
+        "starts-with" ; string starts with
+        "ends-with"   ; string ends with
+        "++"          ; append lists
+        ))
+
+(defvar nushell-ts-mode--keywords)
+(setq nushell-ts-mode--keywords
+      '("alias"
+        "def"
+        "let"
+        "source"))
+
+(defvar nushell-ts-mode--types)
+(setq nushell-ts-mode--types
+      '("any"
+        "block"
+        "bool"
+        "cell-path"
+        "cond"
+        "duration"
+        "error"
+        "expr"
+        "filesize"
+        "glob"
+        "int"
+        "list"
+        "math"
+        "number"
+        "operator"
+        "path"
+        "range"
+        "record"
+        "signature"
+        "string"
+        "table"
+        "variable"))
+
+(defvar nushell-ts-mode--indent-rules)
+(setq nushell-ts-mode--indent-rules
+      `((nu
+         ((node-is ")") parent-bol 0)
+         ((node-is "]") parent-bol 0)
+         ((node-is "}") parent-bol 0)
+         ((parent-is "block") parent-bol nushell-ts-mode-indent-offset)
+         ((parent-is "string") parent-bol nushell-ts-mode-indent-offset)
+         ((parent-is "array") parent-bol nushell-ts-mode-indent-offset)
+         ((parent-is "val_list") parent-bol nushell-ts-mode-indent-offset)
+         ((parent-is "expr_parenthesized") parent-bol nushell-ts-mode-indent-offset)
+         (no-node parent-bol 0))))
+
+(defun nushell-ts-mode--defun-name (node)
+  "Return the defun name of NODE.
+Return nil if there is no name or if NODE is not a defun node."
+  (pcase (treesit-node-type node)
+    ("stmt_let"
+     (treesit-node-text
+      (treesit-node-child node 1) t))
+    ("assignment"
+     (treesit-node-text
+      (treesit-node-child-by-field-name node "lhs") t))
+    ("decl_def"
+     (treesit-node-text
+      (treesit-node-child-by-field-name node "name") t))))
+
+(defvar nushell-ts-mode--font-lock-settings "Font-lock settings for Nushell.")
+(setq nushell-ts-mode--font-lock-settings
+      (treesit-font-lock-rules
+       :language 'nu
+       :feature 'property
+       '((record_entry (identifier) @font-lock-property-name-face))
+
+       :language 'nu
+       :feature 'comment
+       '([(comment) (shebang)] @font-lock-comment-face)
+
+       :language 'nu
+       :feature 'bracket
+       '((["(" ")" "[" "]" "{" "}"]) @font-lock-bracket-face)
+
+       :language 'nu
+       :feature 'definition
+       '((stmt_let (identifier) @font-lock-variable-name-face)
+         (assignment (val_variable) @font-lock-variable-name-face)
+         (decl_def (cmd_identifier) @font-lock-function-name-face)
+         (parameter (identifier) @font-lock-variable-name-face))
+
+       :language 'nu
+       :feature 'function
+       '((command (cmd_identifier) @font-lock-function-call-face)
+         (decl_alias (cmd_identifier) @font-lock-function-call-face))
+
+       :language 'nu
+       :feature 'keyword
+       `([,@nushell-ts-mode--keywords] @font-lock-keyword-face)
+
+       :language 'nu
+       :feature 'number
+       '((val_number)
+         @font-lock-number-face)
+
+       :language 'nu
+       :feature 'operator
+       `([,@nushell-ts-mode--operators] @font-lock-operator-face)
+
+       :language 'nu
+       :feature 'string
+       '((val_string) @font-lock-string-face)
+
+       :language 'nu
+       :feature 'types
+       `([,@nushell-ts-mode--types] @font-lock-type-face)
+
+       :language 'nu
+       :feature 'variable
+       '((val_variable) @font-lock-variable-use-face)
+
+       :language 'nu
+       :feature 'error
+       :override t
+       '((ERROR) @font-lock-warning-face)))
+
+(define-derived-mode nushell-ts-mode prog-mode "NuShell"
+  "Major mode for editing NuShell scripts."
+  :group 'nushell
+  ;; :syntax-table nushell-ts-mode--syntax-table
+  (when (treesit-ready-p 'nu)
+    (treesit-parser-create 'nu)
+
+    ;; Comments
+    (setq-local comment-start "#")
+    (setq-local comment-end "")
+
+    ;; Indent.
+    (setq-local treesit-simple-indent-rules nushell-ts-mode--indent-rules)
+
+    ;; Navigation.
+    (setq-local treesit-defun-type-regexp
+                (regexp-opt '("decl_def"
+                              "stmt_let"
+                              "assignment")))
+    (setq-local treesit-defun-name-function #'nushell-ts-mode--defun-name)
+
+    ;; Font-lock.
+    (setq-local treesit-font-lock-settings nushell-ts-mode--font-lock-settings)
+    (setq-local treesit-font-lock-feature-list
+                '(( comment definition)
+                  ( error keyword string types)
+                  ( function number operator property variable)
+                  ( bracket )))
+
+    ;; Imenu.
+    (setq-local treesit-simple-imenu-settings
+                `(("Variable" "\\`stmt_let\\'" nil nil)
+                  ("Variable" "\\`assignment\\'" nil nil)
+                  ("Function" "\\`decl_def\\'" nil nil)))
+
+    (setq-local electric-indent-chars
+                (append "{}()[]" electric-indent-chars))
+    (setq-local electric-layout-rules
+                '((?\; . after) (?\{ . after) (?\} . before)))
+
+    (treesit-major-mode-setup)))
+
+(when (treesit-ready-p 'nu)
+  (add-to-list 'auto-mode-alist '("\\.nu\\'" . nushell-ts-mode))
+  (add-to-list 'interpreter-mode-alist '("nu" . nushell-ts-mode)))
+
+(provide 'nushell-ts-mode)
+
+;;; nushell-ts-mode.el ends here
